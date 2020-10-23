7ffd16c06766d9ead478e89802d332a17bd9e06c
mahendrafajar17 <mahendrafajar17@gmail.com>
2020-10-23 09:09:45 +0700

    coba

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -58,7 +58,7 @@ p {
 <body>
 	<div id="container">
 		<h1><?php echo filter_var($heading, FILTER_SANITIZE_FULL_SPECIAL_CHARS); ?></h1>
-		<?php echo $message; ?>
+		<?php echo filter_var($message, FILTER_SANITIZE_FULL_SPECIAL_CHARS); ?>
 	</div>
 </body>
 </html>
