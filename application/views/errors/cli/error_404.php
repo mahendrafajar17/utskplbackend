2165a867c6b6216bb101dfee5f33ce6083067af3
mahendrafajar17 <mahendrafajar17@gmail.com>
2020-10-23 10:32:27 +0700

    new push

--- application/views/errors/cli/error_404.php
+++ application/views/errors/cli/error_404.php
@@ -1,8 +1,8 @@
 <?php
 defined('BASEPATH') ;
 
-echo filter_var(("\nERROR: ".
-	$heading.
-	"\n\n".
-	$message.
-	"\n\n"), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+echo "\nERROR: ",
+	filter_var($heading, FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+	"\n\n",
+	filter_var($message, FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+	"\n\n";
